15b2e2f122cc08d50e8f7b907962084ef5f92579
saasbook <dwu7401@gmail.com>
2014-02-19 17:06:43 -0800

    progress is being made

--- code/user_similarity.py
+++ code/user_similarity.py
@@ -1,6 +1,10 @@
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
 
 class UserSimilarity(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
@@ -12,6 +16,15 @@ class UserSimilarity(MRJob):
     # reviewed businesses
     ##/
 
+    def extract_business(self, _, record):
+        if record['type'] == 'review':
+            yield [record['user_id'], record['business_id']]
+
+    def business_set(self, uid, bid): #[user_id, business_id]
+        business_ids = set(bid)
+        print "uid: %s bid: %s" % (str(uid), str(len(business_ids)))
+        yield [uid, business_ids] 
+
     def steps(self):
         """TODO: Document what you expect each mapper and reducer to produce:
         mapper1: <line, record> => <key, value>
@@ -19,8 +32,9 @@ class UserSimilarity(MRJob):
         mapper2: ...
         """
         return [
-            self.mr(mapper=self.mapper1, reducer=self.reducer1),
-            self.mr(mapper=...),
+            self.mr(self.extract_business, self.business_set),
+#            self.mr(mapper=self.mapper1, reducer=self.reducer1),
+#            self.mr(mapper=...),
         ]
 
 
